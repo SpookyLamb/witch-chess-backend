1b63c7f2e367ed7e0224ebcaa706d98a1aee0dba
SpookyLamb <turtleytyrant@gmail.com>
2024-06-12 09:37:45 -0400

    Authentication.

--- witch_chess/urls.py
+++ witch_chess/urls.py
@@ -14,9 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path
+from rest_framework_simplejwt.views import (
+  TokenObtainPairView,
+  TokenRefreshView,
+)
+from witch_chess_app.views import *
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create-user/', create_user),
+    path('profile/', get_profile),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('token/', TokenObtainPairView.as_view()),
 ]
--- witch_chess_app/admin.py
+++ witch_chess_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+from witch_chess_app.models import *
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Profile, ProfileAdmin)
